d0588f158ba1a6f2246dc935595f666d01272fc5
haruoSugano <haruosugano@icloud.com>
2022-08-19 00:16:41 -0300

    Corrigindo a configuracao para os testes serem executado

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserCase.spec.ts
@@ -1,6 +1,6 @@
 import { AppError } from "@errors/AppError";
 import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
-import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
 import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
 import { AuthenticateUseCase } from "./AuthenticateUseCase"
 
