ef86ea85e2a51be07ac89e6a9229a752612eaa69
haruoSugano <haruosugano@icloud.com>
2022-08-21 18:09:32 -0300

    Criando seed de usuario

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,16 @@ import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import "express-async-errors";
 
-import './shared/infra/typeorm';
 import "./shared/container";
 
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from './swagger.json';
 
 import { AppError } from '@shared/errors/AppError';
+import createConnection from '@shared/infra/typeorm';
 import { router } from '@shared/infra/http/routes';
 
+createConnection();
 const app = express();
 
 app.use(express.json());
